11e0137dc8c2d9c143ed0f8bd105e19f1f347b61
amarafioti <andimarafioti@gmail.com>
2020-01-20 15:28:50 +0100

    fix a few more bugs

--- utils.py
+++ utils.py
@@ -3,20 +3,20 @@ import librosa
 
 
 # This function might need another name
-def preprocess_signal(y, M=1024): 
+def preprocess_signal(y, M=1024):
     """Trim and cut signal"""
     # Trimming
     y, _ = librosa.effects.trim(y)
-    
+
     # Preemphasis
     # y = np.append(y[0], y[1:] - 0.97 * y[:-1])
-    
+
     # Padding
-    left_over = np.mod(y, M)
-    extra = M-left_over if left_over else 0
-    y = np.pad(y,(0,extra))
-    assert(np.mod(len(y),M)==0)
-    
+    left_over = np.mod(len(y), M)
+    extra = M - left_over
+    y = np.pad(y, (0, extra))
+    assert (np.mod(len(y), M) == 0)
+
     return y
 
 
@@ -27,10 +27,7 @@ def load_signal(fpath, sr=None):
 
 
 def downsample_tf_time(mel, rr):
-    tmp = np.zeros([mel.shape[0], mel.shape[1]//rr], tmp.dtype)
+    tmp = np.zeros([mel.shape[0], mel.shape[1] // rr], mel.dtype)
     for i in range(rr):
         tmp += mel[:, i::rr]
-    return tmp/rr
-
-
-    
+    return tmp / rr
