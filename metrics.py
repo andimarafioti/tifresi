f1b93fda54dc2011a0cf2d1ef11bc419913650c6
Nathanael Perraudin <nperraud@users.noreply.github.com>
2020-01-21 16:35:45 +0100

    add projection loss

--- metrics.py
+++ metrics.py
@@ -3,7 +3,7 @@ import numpy as np
 __author__ = 'Andres'
 
 
-def projectionLoss(targetSpectrogram, originalSpectrogram):
-    magnitudeError = np.linalg.norm(np.abs(targetSpectrogram) - np.abs(originalSpectrogram), 'fro') / \
-    np.linalg.norm(np.abs(targetSpectrogram), 'fro')
-    return 20 * np.log10(1 / magnitudeError)
+def projection_loss(target_spectrogram, original_spectrogram):
+    magnitude_error = np.linalg.norm(np.abs(target_spectrogram) - np.abs(original_spectrogram), 'fro') / \
+    np.linalg.norm(np.abs(target_spectrogram), 'fro')
+    return 20 * np.log10(1 / magnitude_error)
