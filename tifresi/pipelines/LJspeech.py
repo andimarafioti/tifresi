51ebc0e81408347280cb6f51908c8cf68b6bd079
Nathanaël Perraudin <nperraud@users.noreply.github.com>
2020-02-17 15:19:11 +0100

    update pipeline

--- tifresi/pipelines/LJspeech.py
+++ tifresi/pipelines/LJspeech.py
@@ -34,10 +34,12 @@ def compute_mag_mel(y):
     assert(np.max(mag)<=1)
     assert(np.min(mag)>=0)    
     
-    mel = log_spectrogram(mel, dynamic_range_dB=p.mel_dynamic_range_dB)/p.mel_dynamic_range_dB+1
     # Reduction rate
     if p.reduction_rate > 1:
-        mel = downsample_tf_time(mel, p.reduction_rate)
+        mel = downsample_tf_time(mel, p.reduction_rate)    
+    
+    mel = log_spectrogram(mel, dynamic_range_dB=p.mel_dynamic_range_dB)/p.mel_dynamic_range_dB+1
+
     # Float32
     mel = mel.astype(np.float32)
     mag = mag.astype(np.float32) 
