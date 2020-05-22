e450844221f280f4507c69cc2f61daa04073f994
Nathanael Perraudin <nperraud@users.noreply.github.com>
2020-05-22 15:52:41 +0200

    update setup + add licence

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 import setuptools
 from setuptools import setup
 
-setup(name='tifresi',
-      version='0.1.1',
-      description='Time Frequency Spectrogram Inversion',
-      url='https://https://github.com/nperraud/stft4pghi',
-      author='Andrés Marafioti, Nathanael Perraudin, Nicki Hollighaus',
-      author_email='nathanael (dot) perraudin (at) sdsc (dot) ethz (dot) ch',
-      license='MIT',
-      packages=setuptools.find_packages(),
-      zip_safe=False)
+setup(
+    name='tifresi',
+    version='0.1.2',
+    description='Time Frequency Spectrogram Inversion',
+    url='https://github.com/andimarafioti/tifresi',
+    author='Andrés Marafioti, Nathanael Perraudin, Nicki Hollighaus',
+    author_email='nathanael (dot) perraudin (at) sdsc (dot) ethz (dot) ch',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    extras_require={'testing': ['flake8', 'pytest', 'jupyterlab']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers', 'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux', 'Programming Language :: C',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering'
+    ],
+    install_requires=[
+        'cython', 'ltfatpy', 'numpy', 'numba', 'librosa', 'matplotlib'
+    ],
+)
